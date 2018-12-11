4f4b3ee902f012aec0f8d0ce732ff68f050ff44c
Хвостов Виктор Владимирович <hvostovvv@gau.local>
2018-12-11 16:12:12 +0300

    dates, mutators and scopes

--- app/Article.php
+++ app/Article.php
@@ -3,12 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Article extends Model
 {
     protected $fillable = [
 		'title',
 		'body',
-		'published_at'
+		'published_at',
+		'excerpt'
 	];
+	protected $dates = ['published_at'];
+	public function setPublishedАtAttribute($date)
+	{
+		$this->attributes['published_at'] = Carbon::createFromFormat('Y-m-d', $date);
+	}
+	// Область запроса(область видимости), позволяет использовать в Controller конструкцию published();
+	public function scopePublished($query)
+	{
+		$query->where('published_at','<=',Carbon::now());
+	}
+	public function scopeUnPublished($query)
+	{
+		$query->where('published_at','>',Carbon::now());
+	}
 }
--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 // Импорт модели
-use Carbon\Carbon; 
-use App\Article; 
+use Carbon\Carbon;
+use App\Article;
 // use Illuminate\Http\Request;
 use Request;
 // use Symfony\Component\HttpFoundation\Request;
@@ -12,34 +12,38 @@ class ArticlesController extends Controller
 {
 	public function index()
 	{
-		$articles = Article::latest('published_at')->get();
+		$articles = Article::latest('published_at')->published()->get();
 		
 		return view('articles.index',compact('articles'));
 	}
 	public function show($id)
 	{
 		$article = Article::findOrFail($id);
+		dd($article->published_at);
 		// dd($article);
 		// if (is_null($article)) {
 		// 	about('404');
-		// }
+		// }			
 		return view('articles.show',compact('article'));
 	}
 	public function create()
 	{
 		return view('articles.create');
-	}
+	}															
 
 	// сохранить новую статью
 	public function store()
 	{
 		// получить все данные из формы
-		$input = Request::all();
-		$input['published_at'] = Carbon::now();
+		$newArticle = Request::all();
+		// $input['published_at'] = Carbon::now();
 		// $article = new Article(['title' => ]);
 		// Article->title = input['title'];
-		Article::create($input);
-		// $input = Request::get('title');
+		$newArticle['published_at'] = Carbon::createFromFormat('Y-m-d', $newArticle['published_at']);
+	
+		Article::create($newArticle);
+		
+
 		return redirect('articles');
 	}
 }
